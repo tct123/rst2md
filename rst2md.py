19707eae33622fcd1c8d06e4d19833319a54ba85
tct123 <42028373+tct123@users.noreply.github.com>
2024-07-07 06:47:25 +0200

    Update rst2md.py

--- rst2md.py
+++ rst2md.py
@@ -1,10 +1,8 @@
 import pypandoc
-from pypandoc.pandoc_download import download_pandoc
 
-download_pandoc()
-input_file = 'README.txt'
-output_file = 'README.md'
+input_file = "README.txt"
+output_file = "README.md"
 
-output = pypandoc.convert_file(input_file, 'md', format='rst')
-with open(output_file, 'w') as f:
+output = pypandoc.convert_file(input_file, "md", format="rst")
+with open(output_file, "w") as f:
     f.write(output)
